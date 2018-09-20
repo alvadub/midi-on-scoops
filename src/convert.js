f59a59e312722b9264e973f2e5c2df13063d2116
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-09-20 03:17:13 -0500

    Support fpr synth and samples; fix

--- src/convert.js
+++ src/convert.js
@@ -3,32 +3,33 @@ const scribble = require('scribbletune');
 const utils = require('./utils');
 const reduce = require('./reduce');
 
-module.exports = ast => {
+module.exports = (ast, samples) => {
   const map = {};
 
   Object.keys(ast.tracks).forEach(key => {
     const context = utils.merge(ast.tracks[key].context, ast.context);
+    const options = ast.tracks[key].settings;
     const props = ast.tracks[key].params;
-    const config = {};
+    const clip = {};
 
     props.forEach(field => {
-      config[field.prop] = reduce(field.input, context);
+      clip[field.prop] = reduce(field.input, context);
     });
 
-    // FIXME: stats?
-    // console.log(key, config.notes.length);
-    // console.log(key, config.pattern.length);
-
-    if (Array.isArray(config.pattern)) {
-      config.pattern = config.pattern.join('');
+    if (Array.isArray(clip.pattern)) {
+      clip.pattern = clip.pattern.join('');
     }
 
-    let clip;
+    let output;
 
     try {
-      clip = scribble.clip(config);
+      output = scribble.clip({
+        samples: samples && samples[options.samples],
+        synth: options.synth,
+        ...clip,
+      });
     } catch (e) {
-      throw new Error(`Invalid clip settings, given '${JSON.stringify(config, null, 2)}'`);
+      throw new Error(`Invalid clip settings, given '${JSON.stringify(output, null, 2)}'`);
     }
 
     if (!ast.tracks[key]) {
@@ -38,7 +39,7 @@ module.exports = ast => {
     map[key] = map[key] || [];
     map[key].push({
       track: key,
-      clipObj: clip,
+      clipObj: output,
       settings: ast.tracks[key].settings,
     });
   });

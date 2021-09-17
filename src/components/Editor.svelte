c2099bfd59bf21a0969351c04f4b4d84a81a39cf
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-09-17 12:50:42 -0500

    wip

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,98 +1,48 @@
 <script>
   import Player from './player.js';
-  import { note, isNote, default as tokenize } from '../lib/tokenize';
+  import { parse } from '../lib/parser';
+
+  // FIXME: implements, first, a good parser for these inputs...
+  // here we'll have 2 axis, horzontally we stack the bars...
+  // vertically we can create layers of those stacks...
+  // then, we can use the Y-axis to play as scenes!
 
   let value = `# skanking demo
 
 ; our variables
 %X  a4|c5|e5
+%Y  g4|d4|a#4
 
-; drumkit
-3   ---- ---- ---- ---- x--- ---- ---- ----
-14  ---- ---- ---- ---- x--- ---- ---- ----
-37  x--- ---- ---- x--- x--- ---- x--- ----
-35  x--- x--- x--- x--- x--- x--- x--- x--- ; after comment
+@scene 1
 
-; chords
-17  ---- ---- x--- ---- ---- ---- x--- ---- %X %
+  ; drumkit
+  ; CH VOL PAN?
+  3       ---- ---- ---- ---- x--- ---- ---- ----
+  14      ---- ---- ---- ---- x--- ---- ---- ----
+  37      x--- x--- ---- x--- x--- ---- x--- ----
+  35      x--- x--- x--- x--- x--- x--- x--- x--- ; after comment
 
+  ; chords
+  15      ---- ---- x--- ---- ---- ---- x--- ---- %X %
+  165     ---- x--- ---- x--- ---- x--- ---- x--- %X % % %
+
+@scene 2
+
+  ; chords
+  ;15  ---- ---- x--- ---- ---- ---- x--- ---- %Y %
+  ;183 ---- x--- ---- x--- ---- x--- ---- x--- %Y % % %
+
+  ; how to handle duration, e.g. x__-
+  ; how to repeat from previous stacks?
+  ; they should merge from previous ones? then extends?
+
+> 1 1 2 1
 `;
 
-
-  function encode(input) {
-    if (Array.isArray(input)) return input.map(x => x && encode(x));
-    if (Array.isArray(input.value)) return encode(input.value);
-
-    if (typeof input === 'object') {
-      console.log('OBJECT', input);
-      return (parseInt(input, 10) || 1) * 12 + encode(input.value);
-    }
-
-    if (!isNote(input)) {
-      console.log('INVALID', input);
-      return -1;
-    }
-    return note(input);
-  }
-
-  function reduce(values, ctx) {
-    return values ? values.reduce((memo, cur) => {
-      if (cur === '%') {
-        memo.push(memo[memo.length - 1]);
-      } else if (typeof cur === 'string' && cur.charAt() === '%') {
-        memo.push(...reduce(ctx[cur], ctx));
-      } else {
-        memo.push(cur);
-      }
-      return memo;
-    }, []) : [];
-  }
-
-  function parse(buffer) {
-    const tracks = {};
-
-    let track;
-    let data = {};
-    buffer.split('\n').forEach(line => {
-      line = line.replace(/;.+?$/, '');
-      if (!line.trim()) return;
-      if (line.charAt() === '%') {
-        const [name, value] = line.split(/\s+/);
-
-        if (value) data[name] = tokenize(scribble, value);
-      } else if (line.charAt() === '#') {
-        if (track) {
-          tracks[track] = data;
-          data = {};
-        }
-
-        track = line.substr(1).trim();
-      } else {
-        const [ins, ...ticks] = line.split(/\s+/);
-        const offset = ticks.findIndex(x => !'x-_'.includes(x.charAt()));
-
-        if (offset > 0) {
-          data[`#${ins}`] = {
-            notes: reduce(ticks.slice(offset), data),
-            pattern: ticks.slice(0, offset).join('').split(''),
-          };
-        } else {
-          data[`#${ins}`] = { pattern: ticks.join('').split('') };
-        }
-      }
-    });
-
-    if (track) {
-      tracks[track] = data;
-    }
-    console.log(tracks);
-    return tracks;
-  }
-
   window.p = window.p || null;
-  setTimeout(() =>  {
+  setTimeout(() => {
     p = window.p || new Player();
-    p.setLoopMachine(getData(value));
+    p.setLoopMachine(parse(value));
   }, 200);
 
   let tempo = (p && p.bpm) || 127;
@@ -106,29 +56,11 @@
     if (p) p.playLoopMachine();
   }
 
-  function getData(text) {
-    const tracks = parse(text);
-    const midi = [];
-
-    Object.keys(tracks).forEach(name => {
-      Object.entries(tracks[name]).forEach(([kind, token]) => {
-        if (kind.charAt() === '#') {
-          const steps = token.pattern.map(x => (x === 'x' ? 127 : 0));
-          const notes = encode(token.notes || []);
-
-          midi.push([parseInt(kind.substr(1), 10), ...steps, ...notes]);
-        }
-      });
-    });
-
-    return midi;
-  }
-
   $: if (p) {
     if (p.bpm !== tempo) {
       p.playLoopMachine(tempo);
     } else {
-      p.setLoopMachine(getData(value));
+      p.setLoopMachine(parse(value));
     }
   }
 </script>
---
+++ src/lib/parser.js
@@ -0,0 +1,88 @@
+import { note, isNote, default as tokenize } from './tokenize';
+
+export function encode(input) {
+  if (Array.isArray(input)) return input.map(x => x && encode(x));
+  if (Array.isArray(input.value)) return encode(input.value);
+
+  if (typeof input === 'object') {
+    console.log('OBJECT', input);
+    return (parseInt(input, 10) || 1) * 12 + encode(input.value);
+  }
+
+  if (!isNote(input)) {
+    console.log('INVALID', input);
+    return -1;
+  }
+  return note(input);
+}
+
+export function reduce(values, ctx) {
+  return values ? values.reduce((memo, cur) => {
+    if (cur === '%') {
+      memo.push(memo[memo.length - 1]);
+    } else if (typeof cur === 'string' && cur.charAt() === '%') {
+      memo.push(...reduce(ctx[cur], ctx));
+    } else {
+      memo.push(cur);
+    }
+    return memo;
+  }, []) : [];
+}
+
+export function parse(buffer) {
+  const tracks = {};
+
+  let track;
+  let data = {};
+  buffer.split('\n').forEach(line => {
+    line = line.replace(/;.+?$/, '').trim();
+    if (!line) return;
+    if (line.charAt() === '%') {
+      const [name, value] = line.split(/\s+/);
+
+      if (value) data[name] = tokenize(scribble, value);
+    } else if (line.charAt() === '#') {
+      if (track) {
+        tracks[track] = data;
+        data = {};
+      }
+
+      track = line.substr(1).trim();
+    } else if (line.charAt() === '>') {
+      console.log('SECTION');
+    } else if (line.charAt() === '@') {
+      console.log('SCENE');
+    } else {
+      const [ins, ...ticks] = line.split(/\s+/);
+      const offset = ticks.findIndex(x => !'x-_'.includes(x.charAt()));
+
+      if (offset > 0) {
+        data[`#${ins}`] = {
+          notes: reduce(ticks.slice(offset), data),
+          pattern: ticks.slice(0, offset).join('').split(''),
+        };
+      } else {
+        data[`#${ins}`] = { pattern: ticks.join('').split('') };
+      }
+    }
+  });
+
+  if (track) {
+    tracks[track] = data;
+  }
+
+  const midi = [];
+
+  Object.keys(tracks).forEach(name => {
+    Object.entries(tracks[name]).forEach(([kind, token]) => {
+      if (kind.charAt() === '#') {
+        const steps = token.pattern.map(x => (x === 'x' ? 127 : 0));
+        const notes = encode(token.notes || []);
+
+        midi.push([parseInt(kind.substr(1), 10), ...steps, ...notes]);
+      }
+    });
+  });
+
+  return midi;
+}
